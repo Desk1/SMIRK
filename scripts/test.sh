cdb5d71fe11599f2b866330dca8570c602ef14a0
Desk1 <cherianjacob2014@gmail.com>
2026-03-07 11:44:35 +0000

    test script

---
+++ scripts/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash --login
+#SBATCH --job-name=test_script # Replace with the name of your job
+#SBATCH --mail-user=m.chung@abdn.ac.uk # use your own email
+#SBATCH --mail-type=BEGIN,END,FAIL # when to send notification email
+# SBATCH -o slurm.%j.out # STDOUT '%j' will be replaced with job-id
+# SBATCH -e slurn.%j.err # STDERR
+#SBATCH --ntasks=1 # The number of tasks
+# (default is 1 cpu per task)
+#SBATCH --time=00:05:00 # length of time your job needs
+#SBATCH --partition=compute # 'queue' job to be submitted to
+
+module load conda
+conda activate SMIRK-HPC
+pwd
