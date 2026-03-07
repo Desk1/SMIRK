58e11b638080ad9001ae1437ab053b551885cea9
Desk1 <cherianjacob2014@gmail.com>
2026-03-07 11:35:21 +0000

    slurm sampling script

---
+++ scripts/hpc-sample.sh
@@ -0,0 +1,31 @@
+#!/bin/bash --login
+#SBATCH --job-name=sample-synthetic-images
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=k.jacob.22@abdn.ac.uk
+#SBATCH --partition=uoa-gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=1:00:00
+
+#SBATCH --output=/sharedscratch/SMIRK/jobs/job_output_%j.log   # Standard output (print statements)
+#SBATCH --error=/sharedscratch/SMIRK/jobs/job_error_%j.log     # Errors/Tracebacks
+
+# activate environment
+module load conda
+conda activate SMIRK-HPC
+cd sharedscratch/SMIRK
+
+# run py script
+source activate pytorch
+python my_sample_z_w_space.py &
+PID=$!
+
+# output nvidia interface
+while ps -p $PID > /dev/null;
+do
+nvidia-smi > smi_0.txt
+sleep 10
+done
