53bc1adf54a4f2b172002dd0d3adbfcf9ad92853
Desk1 <cherianjacob2014@gmail.com>
2026-03-09 11:58:49 +0000

    test

--- scripts/hpc-sample.sh
+++ scripts/hpc-sample.sh
@@ -4,7 +4,8 @@
 #SBATCH --mail-user=k.jacob.22@abdn.ac.uk
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:3g.20gb:1
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=gpu02
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=0
