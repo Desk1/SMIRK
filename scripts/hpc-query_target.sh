5cb245155140f45183bdb1e5d4c1cacec6c94669
Desk1 <cherianjacob2014@gmail.com>
2026-03-09 12:13:42 +0000

    query target model

---
+++ scripts/hpc-query_target.sh
@@ -0,0 +1,30 @@
+#!/bin/bash --login
+#SBATCH --job-name=query-blackbox-model
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=k.jacob.22@abdn.ac.uk
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=gpu02
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=0
+#SBATCH --time=1:00:00
+
+#SBATCH --output=jobs/job_output_%j.log
+#SBATCH --error=jobs/job_error_%j.log  
+
+
+# activate environment
+conda activate SMIRK-HPC
+
+# run py script
+python my_generate_blackbox_attack_dataset.py --arch_name inception_resnetv1_vggface2 vggface2 celeba_partial256 &
+PID=$!
+
+# output nvidia interface
+while ps -p $PID > /dev/null;
+do
+nvidia-smi > smi_0.txt
+sleep 10
+done
