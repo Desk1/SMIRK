444843b62009c22fd519472d90b7c766e20fcba2
Desk1 <cherianjacob2014@gmail.com>
2026-03-09 15:29:20 +0000

    fix

--- scripts/hpc-unzip_dataset.sh
+++ scripts/hpc-unzip_dataset.sh
@@ -4,10 +4,10 @@
 #SBATCH --mail-user=k.jacob.22@abdn.ac.uk
 #SBATCH --partition=compute
 #SBATCH --ntasks=1
-#SBATCH --time=1:00:00
+#SBATCH --time=12:00:00
 
 #SBATCH --output=jobs/job_output_%j.log
 #SBATCH --error=jobs/job_error_%j.log  
 
-
-unzip datasets/vggface2.zip
+cd datasets
+unzip -q vggface2.zip
