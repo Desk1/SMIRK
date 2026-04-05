51727bf8849a3a7094b4c21b758d4ea69d88fb39
Desk1 <cherianjacob2014@gmail.com>
2026-04-05 19:12:40 +0100

    typo

--- smirk/utils/files.py
+++ smirk/utils/files.py
@@ -12,9 +12,9 @@ from pathlib import Path
 # File handling #
 #################
 
-def get_path(relative: str):
+def get_path(relative_location: str):
     PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
-    return str(PROJECT_ROOT / relative)
+    return str(PROJECT_ROOT / relative_location)
 
 # todo: rewrite
 def create_folder(folder):
