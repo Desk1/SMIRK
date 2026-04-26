fdee4a961c8019878c06146768573703e10983e6
Desk1 <cherianjacob2014@gmail.com>
2026-04-26 09:18:48 +0100

    hpc sample script

---
+++ scripts/hpc-sample.sh
@@ -0,0 +1,31 @@
+#!/bin/bash --login
+#SBATCH --job-name=sample-synthetic-images
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=k.jacob.22@abdn.ac.uk
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=gpu02
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=0
+#SBATCH --time=1:00:00
+
+#SBATCH --output=jobs/job_output_%j.log
+#SBATCH --error=jobs/job_error_%j.log  
+
+
+
+# activate environment
+conda activate SMIRK-HPC
+
+# run py script
+python -m smirk.scripts.sample &
+PID=$!
+
+# output nvidia interface
+while ps -p $PID > /dev/null;
+do
+nvidia-smi > output/smi_0.txt
+sleep 10
+done
