546fae78b89bcaaa34838dce578b78d5ce72e503
Desk1 <cherianjacob2014@gmail.com>
2026-04-27 10:20:58 +0100

    omg the gpu is free

---
+++ scripts/hpc-train_surrogate_gpu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --login
+#SBATCH --job-name=MI_surrogate_train
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=k.jacob.22@abdn.ac.uk
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=gpu02
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=0
+#SBATCH --time=24:00:00
+
+#SBATCH --output=jobs/job_output_%j.log
+#SBATCH --error=jobs/job_error_%j.log  
+
+
+# activate environment
+conda activate SMIRK-HPC
+
+# run scripts
+python -m smirk.scripts.train_surrogate
+
