e0d2233169386c83d4bc90f23734fcfbcc0f289e
Desk1 <cherianjacob2014@gmail.com>
2026-04-27 14:55:03 +0100

    das2

--- SMILE-baseline/long-tailed_surrogate_training.py
+++ SMILE-baseline/long-tailed_surrogate_training.py
@@ -434,7 +434,7 @@ def main(args):
                 Normalize(Mean, Std)
             ])
 
-        totalset = torchvision.datasets.ImageFolder(os.path.join(_base, "datasets", "vggface2", "train"), transform=test_transform)
+        totalset = torchvision.datasets.ImageFolder("./datasets/vggface2/train", transform=test_transform)
         trainset_list, testset_list = train_test_split(list(range(len(totalset.samples))), test_size=0.01, random_state=666)
         trainset_no_use= Subset(totalset, trainset_list)
         testset= Subset(totalset, testset_list)
