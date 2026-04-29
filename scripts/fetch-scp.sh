528c690a8ce5bb3dea8d44ece5a4ee6c09c5a3c3
Desk1 <cherianjacob2014@gmail.com>
2026-04-29 23:02:56 +0100

    producing results..

--- scripts/fetch-scp.sh
+++ scripts/fetch-scp.sh
@@ -9,4 +9,6 @@ fi
 
 REMOTE_PATH="$1"
 
-scp -P 1024 "u03kj22@127.0.0.1:/home/u03kj22/sharedscratch/SMIRK/${REMOTE_PATH}" ./
+TEMP="SMILE-baseline/blackbox_attack/ours-surrogate_model/inception_resnetv1_vggface2-celeba_partial256-NGOpt-2500-2500-SMILE-400-0.2-inception_resnetv1_casia-vggface2->CASIA-3/LOGS"
+
+scp -P 1024 "u03kj22@127.0.0.1:/home/u03kj22/sharedscratch/SMIRK/${TEMP}/${REMOTE_PATH}" "./${TEMP}/${REMOTE_PATH}"
